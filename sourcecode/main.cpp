0c0934c078485c9af31e009c537dccd1e7d325cd
Donald Franklin <df0004@morningside.edu>
2014-09-23 17:21:19 -0500

    Add commands for main.cpp
    Added a list of commands for main.ccp to handle. Also, added session
    checking and added main.cpp to the makefile.

--- sourcecode/main.cpp
+++ sourcecode/main.cpp
@@ -6,6 +6,14 @@
 #include <ctime>
 #include "libs/common.hpp"
 
+const int pd_SKEY = 0,
+		  pd_COMMAND = 1;
+const string COMMAND_GAMELIST = "GAMES",
+			 COMMAND_SESSIONLIST = "SESSIONS",
+			 COMMAND_STARTGAME = "START",
+			 COMMAND_GAMEUPDATE = "UPDATE",
+			 COMMAND_CHANGEUSER = "CHANGEUSER";
+
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -25,6 +33,30 @@ int main(int argc, char* argv[])
 	tokenizeStr(postText, DLM, postData);
 
 	cout << "Content-Type: text/plain\n\n";
+	
+	//** Confirm logged in
+	dbResult = checkSession(db, userData[pd_SKEY].c_str()) != DB_SUCCESS;
+	if (dbResult != DB_SUCCESS){
+		cout << "ERROR" << DLM << "Failed to check session id [" << dbResult << "]" << endl;
+		return 0;
+	}
+	if (db.numRows() == 0){
+		cout << "ERROR" << DLM << "Not logged in.";
+		return 0;
+	}
+	
+	//** Handle commands
+	if (postData[pd_COMMAND] == COMMAND_GAMELIST){
+		//** Send list of games
+	} else if (postData[pd_COMMAND] == COMMAND_SESSIONLIST){
+		//** Send list of sessions for a game
+	} else if (postData[pd_COMMAND] == COMMAND_STARTGAME){
+		//** Initialize a game session
+	} else if (postData[pd_COMMAND] == COMMAND_GAMEUPDATE){
+		//** Send list of players in a game and ok message if all slots are taken
+	} else if (postData[pd_COMMAND] == COMMAND_CHANGEUSER){
+		//** Change slot of player in game
+	}
 	
 	return 0;
 }
