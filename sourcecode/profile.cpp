914560fd5a7139c2c6127d4437dac89959ccbb32
Donald Franklin <df0004@morningside.edu>
2014-09-24 02:38:30 -0500

    Add initial profile.cpp data sending
    Profile.cgi will now send a small list of initial data upon request.
    More can be added if requested.

--- sourcecode/profile.cpp
+++ sourcecode/profile.cpp
@@ -16,8 +16,9 @@ const string COMMAND_MYPROFILE = "SHOWME",
 			 COMMAND_OTHERPROFILE = "SHOWOTHER",
 			 COMMAND_COLOR = "COLOR";
 			 
-//** Load profile input
-const int pd_PROFID = 2;
+//** Other fields
+const int pd_PROFID = 2,
+		  pc_COLOR = 2;
 
 int main(int argc, char* argv[])
 {
@@ -27,9 +28,13 @@ int main(int argc, char* argv[])
 		   sendStr = "",
 		   tmpName,
 		   tmpLevel,
+		   tmpBan,
+		   tmpMute,
 		   tmpID,
-		   userID,
-		   userQuery = "SELECT Username, UserLevel FROM HubUsers WHERE UserID = ? LIMIT 1";
+		   tmpRegCode,
+		   tmpFname,
+		   tmpLname,
+		   userID;
 	int dbResult = 0;
 	time_t currentTime = time(0);
 	long int profileID = 0;
@@ -66,17 +71,25 @@ int main(int argc, char* argv[])
 	if (userData[pd_COMMAND] == COMMAND_MYPROFILE || userData[pd_COMMAND] == COMMAND_OTHERPROFILE){
 		//** Load profile of this user
 		if (userData[pd_COMMAND] == COMMAND_OTHERPROFILE){
+			//** Confirm client send id
+			if (userData.size() < 3){
+				cout << "ERROR" << DLM << "Invalid command" << endl;
+				return 0;
+			}
+	
 			profileID = strtol(userData[pd_PROFID].c_str(), NULL, 10);
 		} else {
 			profileID = strtol(userID.c_str(), NULL, 10);
 		}
 		
-		db.prepare(userQuery);
+		queryStr = "SELECT Username, UserLevel, BanLevel, MuteLevel, RegistrationCode, FirstName, LastName FROM HubUsers WHERE UserID = ? LIMIT 1";
+		
+		db.prepare(queryStr);
 		db.bind(1, profileID);
 		
 		//** Run command and send output
 		if (db.runPrepared() != DB_SUCCESS) {
-			cout << "ERROR" << DLM << "Invalid query \"" << userQuery << "\"" << endl;
+			cout << "ERROR" << DLM << "Invalid query \"" << queryStr << "\"" << endl;
 			return 0;
 		}
 		
@@ -88,12 +101,36 @@ int main(int argc, char* argv[])
 		
 		tmpName = db[0][0];
 		tmpLevel = db[0][1];
+		tmpBan = db[0][2];
+		tmpMute = db[0][3];
 		
-		cout << "LOAD" << DLM << tmpName << DLM << tmpLevel << endl;
+		//** Only return a registration code if the user level is high enough
+		if (tmpLevel != "1" && tmpLevel != "0"){
+			tmpRegCode = "";
+		} else {
+			tmpRegCode = db[0][4];
+		}
+		
+		tmpFname = db[0][5];
+		tmpLname = db[0][6];
+		
+		cout << "LOAD" << DLM << tmpName <<
+						DLM << tmpLevel <<
+						DLM << tmpBan <<
+						DLM << tmpMute <<
+						DLM << tmpRegCode << 
+						DLM << tmpFname <<
+						DLM << tmpLname << endl;
 		return 0;
 	} else if (userData[pd_COMMAND] == COMMAND_COLOR){
-		//** Error checking (force user to send skey, command, and userid)
+		//** Error checking (force user to send skey, command, and color)
 		if (userData.size() < 3){
+			cout << "ERROR" << DLM << "Invalid command" << endl;
+			return 0;
+		}
+		
+		//** Force color size to be exactly 6 characters
+		if (userData[pc_COLOR].size() < 6){
 			cout << "ERROR" << DLM << "Invalid command" << endl;
 			return 0;
 		}
