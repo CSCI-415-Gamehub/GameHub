4ef781cc382eb630e6c0756fd78974c5204ce1b5
Donald Franklin <df0004@morningside.edu>
2014-10-03 02:10:11 -0500

    Fixed createdb.sql
    Some characters were mysteriously removed from working lines and some
    other things were changed to match previous versions. I'm not sure how
    this happened, honestly.

--- sourcecode/db/createdb.sql
+++ sourcecode/db/createdb.sql
@@ -1,34 +1,35 @@
-CREATE TABLE HubUsers
-(UserID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS HubUsers
+(
+UserID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 FirstName VARCHAR (20),
 LastName VARCHAR(20),
 Username VARCHAR (20) COLLATE NOCASE,
-Password VARCHAR (20),
+Password VARCHAR (40),
 PasswordSalt VARCHAR (20),
 Email VARCHAR (30),
 RegistrationCode INTEGER,
 RegistrationID INTEGER,
-InviteID VARCHAR (20),
+InviteID INTEGER,
 UserLevel VARCHAR (20),
 BanLevel INTEGER,
 MuteLevel INTEGER,
 Color VARCHAR (6),
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-CREATE TABLE HubMessages
-(Username VARCHAR (20),
+CREATE TABLE IF NOT EXISTS HubMessages
+(
+Username VARCHAR (20),
 Msg VARCHAR (200),
 GameField VARCHAR (50),
 Color VARCHAR (6),
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-CREATE TABLE Games
+CREATE TABLE IF NOT EXISTS Games
 (
 GameID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 GameName Varchar(30) COLLATE NOCASE,
-GameAddress Varchar(30),
 PosterID Integer,
 MinUsers Integer,
 MaxUsers Integer,
@@ -36,45 +37,45 @@ URL Varchar(1042),
 Description Varchar(1041),
 TimesPlayed integer, 
 Ratings integer,
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-Create Table Sessions 
+CREATE TABLE IF NOT EXISTS Sessions
 (
 SessionID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-GameID INTEGER,
 UserID INTEGER,
 UserLevel INTEGER,
-UpdateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL,
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+UpdateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL,
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-Create Table GameSessions 
+Create Table IF NOT EXISTS GameSessions
 (
 SessionID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 GameID INTEGER,
 UserID INTEGER,
-Password VARCHAR (20),
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+Password VARCHAR (40),
+PasswordSalt VARCHAR (20),
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-CREATE TABLE RecentlyPlayed
+CREATE TABLE IF NOT EXISTS RecentlyPlayed
 (
 GameID INTEGER,
 UserID INTEGER,
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
 
-CREATE TABLE Settings
+CREATE TABLE IF NOT EXISTS Settings
 (
 KeyID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-KeyName VARCHAR(10),
-KeyValue VARCHAR(10)
+KeyName VARCHAR(20),
+KeyValue VARCHAR(40)
 );
 
-CREATE TABLE AdminLog
+CREATE TABLE IF NOT EXISTS AdminLog
 (
 UserID INTEGER,
 LogText VARCHAR(300),
-CreateTime DATETIME DEFAULT strftime('%s', 'now') NOT NULL
+CreateTime DATETIME DEFAULT (strftime('%s', 'now')) NOT NULL
 );
